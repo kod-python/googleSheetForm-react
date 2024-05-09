e8072fdf0b214ee47010bececce6d1c274c1f1c6
kod-python <kelalice931@gmail.com>
2024-05-09 12:11:24 +0000

    first comm

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
 import React from "react";
+import "./App.css"
 
 const LinkButton = ({ url, label }) => {
   const handleClick = () => {
     window.location.href = url;
   };
 
-  return <button onClick={handleClick}>{label}</button>;
+  return <button onClick={handleClick} className="btn">{label}</button>;
 };
 
 function App() {
   return (
     <div className="App">
-      <LinkButton url="" label="Click" />
+      <LinkButton url="https://github.com/kod-python" label="Login" />
       
     </div>
   );
